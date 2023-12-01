aadeb2f57a3fbcde9effd3f4c67a56acfa53db84
Mohammad Anar <ai.anarul4232@gmail.com>
2023-12-02 03:46:05 +0600

    jwt verify complete

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const app = express();
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const secret = "veryveridjfdklsjfdsjfjdkdljfkjtodljfh"
+
+const verifyToken = async (req, res, next) => {
+  const bearerToken = req.headers?.authorization;
+  if (!bearerToken) {
+    res.status(403).send("forbidden access")
+  }
+  const token = bearerToken.split(" ")[1];
+  jwt.verify(token, secret, (error, decoded) => {
+    if(error){
+      res.status(403).send("forbidden access")
+    }else{
+      res.user = decoded?.user;
+      next();
+    }
+  })
+  
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zav38m0.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,17 +53,29 @@ async function run() {
     const cuponCollection = client.db("prh-a12").collection("cupons");
     const paymentCollection = client.db("prh-a12").collection("payments");
 
+
+
+
+    // jwt 
+    app.post("/access-token", async (req, res) => {
+      const user  = req.body;
+      const token = jwt.sign(user, secret, {expiresIn:"5h"});
+      res.send(token)
+    })
+
+
+    // apartments api
     app.get("/apartments", async (req, res) => {
       const result = await apartmentCollection.find().toArray();
       res.send(result);
     });
     // agreements apis
-    app.get("/agreements", async (req, res) => {
+    app.get("/agreements", verifyToken, async (req, res) => {
       const result = await agreementCollection.find().toArray();
       res.send(result);
     });
 
-    app.get("/agreements", async (req, res) => {
+    app.get("/agreements", verifyToken, async (req, res) => {
       const query = { userEmail: req?.query?.email };
       const result = await agreementCollection.find(query).toArray();
       res.send(result);
@@ -84,7 +115,7 @@ async function run() {
       res.send(result);
     });
     // cupons apis
-    app.get("/cupons", async (req, res) => {
+    app.get("/cupons",verifyToken, async (req, res) => {
       const result = await cuponCollection.find().toArray();
       res.send(result);
     });
@@ -95,12 +126,12 @@ async function run() {
       console.log(cupon);
     });
     // user related api
-    app.get("/users", async (req, res) => {
+    app.get("/users",verifyToken, async (req, res) => {
       const query = { role: "member" };
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
-    app.get("/users/:email", async (req, res) => {
+    app.get("/users/:email",verifyToken, async (req, res) => {
       const email = req.params?.email;
       const query = { email };
       const result = await userCollection.find(query).toArray();
@@ -136,7 +167,7 @@ async function run() {
       res.send(result);
     });
     // payment related api
-    app.get("/payments/:email", async (req, res) => {
+    app.get("/payments/:email", verifyToken,async (req, res) => {
       const email = req?.params?.email;
       const query = {
         email: email,
